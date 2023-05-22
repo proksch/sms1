b807f190c1e91278de9dc1a97fdb3106795ee465
Luis Cruz <luismirandacruz@gmail.com>
2023-05-22 12:27:22 +0200

    get_data script to a prev directory

--- src/get_data.py
+++ src/get_data.py
@@ -7,7 +7,7 @@ import zipfile
 # URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
 URL = 'https://surfdrive.surf.nl/files/index.php/s/OZRd9BcxhGkxTuy/download' # v2, 2000 datapoints
 
-EXTRACT_DIR = "dataset"
+EXTRACT_DIR = "smsspamcollection"
 
 zip_path, _ = urllib.request.urlretrieve(URL)
 with zipfile.ZipFile(zip_path, "r") as f:
